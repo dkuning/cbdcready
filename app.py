ecf550228d5c58e12507e722babe8e88ed03ca02
dkuning <kuning.denis@gmail.com>
2025-11-30 19:37:29 +0300

    fix

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 # app.py
 from flask import Flask, request, jsonify, render_template, session, redirect, url_for
-import requests
 import pyotp
 import os
 import modules.legalDetails as legalDetails
-from prometheus_client import Counter, Histogram, generate_latest
-import time
+import logging
+
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
@@ -14,6 +13,12 @@ app.secret_key = os.environ.get('FLASK_SECRET_KEY', 'JROFFXC5EVY6N2DY')
 
 # Получаем TOTP-секрет из переменной окружения
 TOTP_SECRET = os.environ.get('TOTP_SECRET', 'DS4LZV52ZI3QNN72')
+
+# Настройка логирования
+LOG_DIR = './logs'
+LOG_FILE = os.path.join(LOG_DIR, 'cbdcReadyApp.log')
+logger = logging.getLogger('cbdcReadyApp')
+logger.setLevel(logging.INFO)
 
 def generate_totp():
     totp = pyotp.TOTP(TOTP_SECRET)
@@ -58,25 +63,7 @@ def logout():
     session.pop('logged_in', None)
     return redirect(url_for('login'))
 
-# Метрики
-REQUEST_COUNT = Counter('http_requests_total', 'Total HTTP requests', ['method', 'endpoint', 'status'])
-REQUEST_LATENCY = Histogram('http_request_duration_seconds', 'HTTP request latency', ['endpoint'])
-
-@app.route('/metrics')
-def metrics():
-    return generate_latest(), 200, {'Content-Type': 'text/plain'}
-
-# Middleware для сбора метрик
-@app.before_request
-def before_request():
-    request.start_time = time.time()
-
-@app.after_request
-def after_request(response):
-    latency = time.time() - request.start_time
-    REQUEST_LATENCY.labels(request.endpoint).observe(latency)
-    REQUEST_COUNT.labels(request.method, request.endpoint, response.status_code).inc()
-    return response
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+    logger.info(f"Приложение запущено на порту {port}")
